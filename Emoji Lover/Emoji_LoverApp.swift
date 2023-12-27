7cd0deda3522c2407ba33f9b5ff5452261377bae
Tristan Khieu <tristankhieu28@gmail.com>
2023-12-27 08:46:22 -0800

    Initial Commit

---
+++ Emoji Lover/Emoji_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverApp.swift
+//  Emoji Lover
+//
+//  Created by Tristan Khieu on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
