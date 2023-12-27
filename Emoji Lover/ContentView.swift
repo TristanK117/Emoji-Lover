01cf92a72dc22196636faa7f3f943003188e025b
Tristan Khieu <tristankhieu28@gmail.com>
2023-12-27 08:56:05 -0800

    Contentview program

--- Emoji Lover/ContentView.swift
+++ Emoji Lover/ContentView.swift
@@ -1,24 +1,43 @@
 //
 //  ContentView.swift
-//  Emoji Lover
+//  EmojiLover
 //
-//  Created by Tristan Khieu on 12/27/23.
+//  Created by Tristan Khieu on 12/21/23.
 //
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case sad = "🥲"
+    case happy = "😃"
+    case love = "😍"
+    case cat = "😹"
+}
+
+
 struct ContentView: View {
+    @State var selection: Emoji = .cat
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers 2")
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
